e010641098a257d87222900992842bdba3068923
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-14 06:48:30 +0800

    Initial Commit

---
+++ Pokemon Go Clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokemon Go Clone
+//
+//  Created by John Crisostomo on 14/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
