1d2bf3d0c80587379167466b970f17dbeea0189d
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-14 07:07:57 +0800

    sets up MapView and the permissions

--- Pokemon Go Clone/ViewController.swift
+++ Pokemon Go Clone/ViewController.swift
@@ -7,19 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager.delegate = self
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print("Ready to go!")
+            mapView.showsUserLocation = true
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
