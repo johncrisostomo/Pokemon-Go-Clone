c6705eaedfeac93b8baa5e86ac11a08634627d85
John Crisostomo <johncrisostomo@protonmail.ch>
2017-04-16 07:00:10 +0800

    adds Core Data helper to seed Pokemons

---
+++ Pokemon Go Clone/CoreDataHelp.swift
@@ -0,0 +1,36 @@
+//
+//  CoreDataHelp.swift
+//  Pokemon Go Clone
+//
+//  Created by John Crisostomo on 16/04/2017.
+//  Copyright © 2017 John Crisostomo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+func addAllPokemon() {
+
+    createPokemon(name: "Mew", imageName: "mew")
+    createPokemon(name: "Meowth", imageName: "meowth")
+    createPokemon(name: "Abra", imageName: "abra")
+    createPokemon(name: "Dratini", imageName: "dratini")
+    createPokemon(name: "Mankey", imageName: "mankey")
+    createPokemon(name: "Bulbasaur", imageName: "bullbasaur")
+    createPokemon(name: "Caterpie", imageName: "caterpie")
+    createPokemon(name: "Weedle", imageName: "weedle")
+    createPokemon(name: "Eevee", imageName: "eevee")
+    createPokemon(name: "Squirtle", imageName: "squirtle")
+    
+    
+    (UIApplication.shared.delegate as! AppDelegate).saveContext()
+}
+
+func createPokemon(name: String, imageName: String) {
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    let pokemon = Pokemon(context: context)
+    
+    pokemon.name = name
+    pokemon.imageName = imageName
+}
